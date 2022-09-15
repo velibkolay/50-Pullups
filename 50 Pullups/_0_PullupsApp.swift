30e6d69a43f4d637a6fd76bb373e6229f9caf73e
velibkolay <veliberkaykolay@gmail.com>
2022-09-15 23:42:33 +0200

    Initial Commit

---
+++ 50 Pullups/_0_PullupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0_PullupsApp.swift
+//  50 Pullups
+//
+//  Created by Veli Berkay Kolay on 15.09.22.
+//
+
+import SwiftUI
+
+@main
+struct _0_PullupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
